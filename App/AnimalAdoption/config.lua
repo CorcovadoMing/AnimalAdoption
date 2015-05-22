46349aad0705af658d9140226b7253da2af59e80
CorcovadoMing <rf37535@gmail.com>
2015-05-22 17:53:55 +0800

    Clean up web app, init ios structure

--- App/AnimalAdoption/config.lua
+++ App/AnimalAdoption/config.lua
@@ -1,32 +1,13 @@
-application =
+application = 
 {
-
-	content =
-	{
-		width = 320,
-		height = 480, 
-		scale = "letterBox",
-		fps = 30,
+	content = 
+	{ 
+		scale = "adaptive",
+		fps = 60,
 		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-		},
-		--]]
-	},
-
-	--[[
-	-- Push notifications
-	notification =
-	{
-		iphone =
-		{
-			types =
-			{
-				"badge", "sound", "alert", "newsstand"
-			}
+		imageSuffix = {
+			["@2x"] = 1.5,
+			["@3x"] = 2.5,
 		}
-	},
-	--]]    
+	}
 }
