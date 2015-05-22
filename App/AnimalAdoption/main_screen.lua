acc7106cca3fd0f59feaa75d8878028c46cb0b79
CorcovadoMing <rf37535@gmail.com>
2015-05-22 22:09:28 +0800

    Complete the main_screen

--- App/AnimalAdoption/main_screen.lua
+++ App/AnimalAdoption/main_screen.lua
@@ -12,9 +12,15 @@ local scene = composer.newScene()
 local image, text1, text2, text3, memTimer
 
 
-local function handleButtonEvent( event )
+local function handleGiveEvent( event )
     if ( "ended" == event.phase ) then
         composer.gotoScene( "scene2", "fade", 400  )
+    end
+end
+
+local function handleGetEvent( event )
+    if ( "ended" == event.phase ) then
+        composer.gotoScene( "scene3", "fade", 0  )
     end
 end
 
@@ -29,24 +35,46 @@ function scene:create( event )
 	image.x = display.contentCenterX
 	image.y = display.contentCenterY
 	image.fill.effect = "filter.exampleBlurGaussian"
-	image.fill.effect.horizontal.blurSize = 50
-	image.fill.effect.vertical.blurSize = 50
+	image.fill.effect.horizontal.blurSize = 10
+	image.fill.effect.vertical.blurSize = 10
 	sceneGroup:insert( image )
+
+	local title = display.newImage( "images/title.png" )
+	title.x = display.contentCenterX
+	title.y = 200
+	sceneGroup:insert( title )
 end
 
 function scene:show( event )	
 	local phase = event.phase	
 	if "did" == phase then
 		button1 = widget.newButton {
-    		id = "button1",
-    		label = "Default",
-    		onEvent = handleButtonEvent
+    		width = 640,
+    		height = 500,
+    		defaultFile = "images/get_btn.png",
+		    overFile = "images/get_btn.png",
+		    label = "",
+		    onEvent = handleGetEvent
 		}
-		button1.x = display.contentCenterX
+		button1.x = display.contentCenterX - 85
 		button1.y = display.contentCenterY
+
+		button2 = widget.newButton {
+    		width = 640,
+    		height = 500,
+    		defaultFile = "images/give_btn.png",
+		    overFile = "images/give_btn.png",
+		    label = "",
+		    onEvent = handleGiveEvent
+		}
+		button2.x = display.contentCenterX - 85
+		button2.y = display.contentCenterY + display.contentCenterY / 2
 
 		-- remove previous scene's view
 		composer.removeScene( "scene4" )
+		
+
+		
 	end	
 end
 
@@ -55,6 +83,7 @@ function scene:hide( event )
 	if "will" == phase then	
 		print( "1: hide event, phase will" )
 		button1.isVisible = false
+		button2.isVisible = false
 	end	
 end
 
--- App/AnimalAdoption/scene2.lua
+++ App/AnimalAdoption/scene2.lua
@@ -5,9 +5,28 @@
 ---------------------------------------------------------------------------------
 
 local composer = require( "composer" )
+local json = require "json"
+local utf8 = require "utf8"
 local scene = composer.newScene()
 
 local image, text1, text2, text3, memTimer
+
+local function networkListener( event )
+
+    if ( event.isError ) then
+        print( "Network error!" )
+    else
+        -- print ( "RESPONSE: " .. event.response )
+        local decoded, pos, msg = json.decode( event.response )
+        if not decoded then
+    		print( "Decode failed at "..tostring(pos)..": "..tostring(msg) )
+		else
+    		for _, v in pairs(decoded) do
+    			text2.text = v.animal_kind
+    		end
+		end
+    end
+end
 
 local function onSceneTouch( self, event )
 	if event.phase == "began" then
@@ -19,6 +38,7 @@ local function onSceneTouch( self, event )
 end
 
 function scene:create( event )
+	network.request( "http://data.coa.gov.tw/Service/OpenData/AnimalOpenData.aspx", "GET", networkListener )
 	local sceneGroup = self.view
 	
 	image = display.newImage( "bg2.jpg" )
@@ -34,7 +54,7 @@ function scene:create( event )
 	text1.x, text1.y = display.contentWidth * 0.5, 50
 	sceneGroup:insert( text1 )
 	
-	text2 = display.newText( "MemUsage: ", 0, 0, native.systemFont, 16 )
+	text2 = display.newText( "MemUsage: ", 0, 0, native.systemFont, 50 )
 	text2:setFillColor( 255 )
 	text2.x, text2.y = display.contentWidth * 0.5, display.contentHeight * 0.5
 	sceneGroup:insert( text2 )
